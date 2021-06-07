9070f79fb6c6d2d5f008f3bdbf8c14a66db596d4
Mischa Holz <me@mhlz.de>
2021-06-07 11:24:14 +0200

    Stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
-set -g fish_user_paths "/Users/mhlz/go/bin" "/usr/local/sbin" $fish_user_paths
+[ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
+set -g fish_user_paths "/home/mhlz/.local/bin" "/home/mhlz/go/bin" "/usr/local/sbin" "/opt/anaconda/bin" $fish_user_paths
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
@@ -12,7 +13,6 @@ set -gx LC_TIME de_DE.UTF-8
 set -gx LC_MONETARY de_DE.UTF-8
 
 
-
 # tabtab source for serverless package
 # uninstall by removing these lines or running `tabtab uninstall serverless`
 [ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
@@ -23,7 +23,12 @@ set -gx LC_MONETARY de_DE.UTF-8
 # uninstall by removing these lines or running `tabtab uninstall slss`
 [ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish
 
-direnv hook fish | source
-
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/mhlz/homefully/sandbox/google-cloud-sdk/path.fish.inc' ]; . '/Users/mhlz/homefully/sandbox/google-cloud-sdk/path.fish.inc'; end
+
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+# eval /opt/anaconda/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,4 @@
 function fish_user_key_bindings
   fzf_key_bindings
+  bind -M insert \cf accept-autosuggestion force-repaint
 end
