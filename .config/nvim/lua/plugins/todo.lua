54c20e97556e019960c2f7769282394cd719593f
Mischa Holz <me@mhlz.de>
2024-07-24 19:05:55 +0200

    Neovim updates

---
+++ .config/nvim/lua/plugins/todo.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/todo-comments.nvim",
+    opts = {
+      highlight = {
+        pattern = [[.*<(KEYWORDS).*:]],
+      },
+      search = {
+        pattern = [[\b(KEYWORDS)\b]],
+      },
+    },
+  },
+}
